81ed2d1e96f7808f54290e9b8bb06e223dc37f5e
theultimategeek444 <ultimate@ultimate.com>
2026-03-14 17:02:51 +0100

    update files

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Serve static files (index.html, style.css, script.js)
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
